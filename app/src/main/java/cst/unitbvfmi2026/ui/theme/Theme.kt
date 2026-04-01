c2c76a7eaad6c4299fca7f42273ac95417cf671a
Bianca Diana <andaras.bianca@gmail.com>
2026-04-01 10:34:32 +0300

    Restore local changes

--- app/src/main/java/cst/unitbvfmi2026/ui/theme/Theme.kt
+++ app/src/main/java/cst/unitbvfmi2026/ui/theme/Theme.kt
@@ -18,9 +18,10 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-	primary = Purple40,
+	primary = PurpleGrey80,
 	secondary = PurpleGrey40,
-	tertiary = Pink40
+	tertiary = Pink40,
+	background = Pink40
 
 	/* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 fun UnitBvFMI2026Theme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	// Dynamic color is available on Android 12+
-	dynamicColor: Boolean = true,
+	dynamicColor: Boolean = false,
 	content: @Composable () -> Unit
 ) {
 	val colorScheme = when {
